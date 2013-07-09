7841da7b6b0b81ca736a3a04d98a1a67f5cea266
Юрий Самарычев <yura.samarychev@digitalizm.ru>
2013-07-09 21:14:57 +0300

    Hello World from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	buffer = fs.readFileSync("index.html");
+  data = buffer.toString();
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
